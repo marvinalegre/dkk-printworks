22bde88f5a344342d6564ee59522b62e865c982b
Marvin Alegre <marvinalegredev@gmail.com>
2024-11-08 19:47:25 +0800

    initial commit

---
+++ src/client/main.jsx
@@ -0,0 +1,10 @@
+// import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <h1>Hello world!</h1>
+  </React.StrictMode>
+);
