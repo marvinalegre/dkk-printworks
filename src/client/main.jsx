32dd3b2fe5a2dd8d09011944f66364b42cf2d1aa
Marvin Alegre <marvinalegredev@gmail.com>
2024-11-08 20:04:06 +0800

    chore: add tailwindcss

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -1,10 +1,10 @@
-// import "./index.css";
+import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1>Hello world!</h1>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
   </React.StrictMode>
 );
