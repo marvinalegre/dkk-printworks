3e7e1c24937a7c59ee455b652141e8bd7454a065
Marvin Alegre <marvinalegredev@gmail.com>
2024-11-14 17:28:44 +0800

    fix: schema rework

--- database/schema.sql
+++ database/schema.sql
@@ -18,56 +18,38 @@ CREATE TABLE users (
     FOREIGN KEY (role_id) REFERENCES roles(role_id)
 );
 
-DROP TABLE IF EXISTS phone_numbers;
-CREATE TABLE phone_numbers (
-    phone_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER NOT NULL,
-    phone_number TEXT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
-
-DROP TABLE IF EXISTS profile_pictures;
-CREATE TABLE profile_pictures (
-    picture_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER NOT NULL,
-    picture_url TEXT NOT NULL,
-    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
-
-DROP TABLE IF EXISTS files;
-CREATE TABLE files (
-    file_id INTEGER PRIMARY KEY AUTOINCREMENT,  
-    user_id INTEGER NOT NULL,                      
-    file_name TEXT NOT NULL,                       
-    file_path TEXT NOT NULL,                       
-    file_size INTEGER NOT NULL,                    
-    num_pages INTEGER NOT NULL,                       
-    file_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
-    FOREIGN KEY (user_id) REFERENCES users(user_id)  
-);
-
 DROP TABLE IF EXISTS orders;
 CREATE TABLE orders (
     order_id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,                           
-    file_id INTEGER NOT NULL,                         
     order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     status TEXT CHECK( status IN ('New', 'Pending', 'In Progress', 'Completed', 'Cancelled') ) NOT NULL DEFAULT 'New', 
     special_instructions TEXT,                          
     total_price DECIMAL(10, 2) NOT NULL,                 
     FOREIGN KEY (user_id) REFERENCES users(user_id),    
-    FOREIGN KEY (file_id) REFERENCES files(file_id) 
+);
+
+DROP TABLE IF EXISTS files;
+CREATE TABLE files (
+    file_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    order_id INTEGER NOT NULL,
+    file_name TEXT NOT NULL,
+    internal_file_name TEXT NOT NULL,
+    file_size INTEGER NOT NULL,
+    num_pages INTEGER NOT NULL,
+    file_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
 );
 
 DROP TABLE IF EXISTS page_ranges;
 CREATE TABLE page_ranges (
     page_range_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    order_id INTEGER NOT NULL,
+    file_id INTEGER NOT NULL,
     page_range TEXT NOT NULL,
     copies INTEGER NOT NULL DEFAULT 1,                 
     paper_size TEXT CHECK( paper_size IN ('A4', 'Short', 'Long') ) NOT NULL DEFAULT 'Short',  
     color TEXT CHECK( color IN ('Colored', 'Black & White') ) NOT NULL DEFAULT 'Black & White', 
     duplex TEXT CHECK( duplex IN ('Single-sided', 'Double-sided') ) NOT NULL DEFAULT 'Single-sided',
-    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+    page_range_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (file_id) REFERENCES files(file_id)
 );
