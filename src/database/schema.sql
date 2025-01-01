98c9d391553527187eaffcb962ea0873c9154af1
Marvin Alegre <marvinalegredev@gmail.com>
2025-01-01 16:53:33 +0800

    use pnpm workspaces

--- src/database/schema.sql
+++
@@ -1,3 +0,0 @@
-PRAGMA defer_foreign_keys=TRUE;
-CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, hashed_password TEXT, jwt_id TEXT UNIQUE);
-CREATE TABLE uploads (id INTEGER PRIMARY KEY, file_name TEXT UNIQUE, user_id INTEGER, FOREIGN KEY (user_id) REFERENCES users(id));
