5aba6a29a651f17ca17de6ffe1bb0d63b37fd32f
Marvin Alegre <marvinalegredev@gmail.com>
2025-01-03 12:00:07 +0800

    fix orderRefNumber var name

--- apps/api/src/main.js
+++ apps/api/src/main.js
@@ -58,7 +58,7 @@ app.get("/api/order", async (req, res) => {
 
   const files = await getOrderFiles(newOrder.order_id);
   res.json({
-    orderRefNum: newOrder.order_reference_number,
+    orderRefNumber: newOrder.order_reference_number,
     files,
   });
 });
